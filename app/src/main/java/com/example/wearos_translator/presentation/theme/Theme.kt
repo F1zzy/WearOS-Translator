7a105f98c11bae3665fbfa41ad2402e58a7b0e18
F1zzy <65316506+F1zzy@users.noreply.github.com>
2025-03-20 02:12:44 +0000

    Creating Git

---
+++ app/src/main/java/com/example/wearos_translator/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.wearos_translator.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WearOSTranslatorTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
